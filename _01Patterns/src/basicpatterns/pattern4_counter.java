9fccd023319c0b2af2afe3654fe7eb428450dce3
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-10 23:07:04 +0530

    counter pattern

---
+++ _01Patterns/src/basicpatterns/pattern4_counter.java
@@ -0,0 +1,22 @@
+package basicpatterns;
+
+public class pattern4_counter {
+
+	public static void main(String[] args) {
+		
+		int count = 1;
+		
+		for(int i = 1 ; i <= 3 ; i++ )
+		{
+			
+			for(int j = 1 ; j <= 3 ; j++) {
+				
+				System.out.print(count + " ");
+				count++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
