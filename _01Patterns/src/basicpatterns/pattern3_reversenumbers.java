db19d7e65058a7d6a36c20b6dac6f3e1e1534d20
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-10 23:02:37 +0530

    reverse number pattern

---
+++ _01Patterns/src/basicpatterns/pattern3_reversenumbers.java
@@ -0,0 +1,21 @@
+package basicpatterns;
+
+public class pattern3_reversenumbers {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i <= 5 ; i++) {
+			
+			for(int j = 5 ; j >=1 ; j --) {
+				
+				System.out.print(j + " ");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}
+
+}
