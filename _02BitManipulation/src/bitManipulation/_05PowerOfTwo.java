8a4c4543fbe9daab3625593ecf3613d19708baf4
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-15 23:09:16 +0530

    element equal to power of 2

--- _02BitManipulation/src/bitManipulation/_05PowerOfTwo.java
+++ _02BitManipulation/src/bitManipulation/_05PowerOfTwo.java
@@ -11,28 +11,41 @@ public class _05PowerOfTwo {
 		System.out.println("Enter a number");
 		int num = sc.nextInt();
 		
-		int n = num-1;
+//		boolean flag = false;
+//		
+//		for(int i = 0; i <= 30 ; i ++)
+//		{
+//			int ans = (int)Math.pow(2, i);
+//			
+//			
+//			if(ans == num) {
+//				flag = true;
+//			}
+//		}
+//		
+//		if(flag) {
+//			System.out.println("number is power of two");
+//		}
+//		else {
+//			System.out.println("number is not power of two");
+//		}
 		
-		int mask = 0;
-		
-		while(n!=0) {
-			mask = (mask << 1) |1;
-			n =  n >> 1;
+		int count = 0;
+		while(num!=0) {
+			if((num & 1) == 1) {
+				count++;
+			}
+			
+			num = num>>1;
+
 		}
 		
-		int ans = ~(num -1)& mask;
-		
-		
-		System.out.println(ans);
-		if( ans == 0)
-		{
-			System.out.println("The number is power of two");
+		if(count==1) {
+			System.out.println("number is power of two");
 		}
-		else
-		{
+		else {
 			System.out.println("number is not power of two");
 		}
-
 	}
 
 }
